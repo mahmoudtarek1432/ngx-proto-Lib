cdbe87bf1fbf11990ed4a3cf95410791985c9394
mahmoudtarek1432 <43779204+mahmoudtarek1432@users.noreply.github.com>
2023-05-07 11:28:51 +0300

    first commit

---
+++ projects/ngx-proto-websocket/src/helper/Endpoint Managment/EndpointFeeder.ts
@@ -0,0 +1,23 @@
+import { IRequest } from './model/IRequest';
+
+export class EndpointFeeder{
+    static FeedRequestEndpoint<obj extends IRequest>(property: obj, endpoint: any): any{
+        let name = Object.getPrototypeOf(property).constructor.name
+        if(endpoint[name] == undefined || endpoint[name] == null){
+            endpoint[name] = new Array<obj>()
+        }
+        endpoint[name].push(property)
+        return endpoint
+    }
+
+    /*static FeedRequestEndpoint<obj extends IRequest>(property: obj, endpoint: any): RequestEndpoints{
+        let propertyNames =  Object.getOwnPropertyNames(endpoint);
+        propertyNames.forEach((n) => {
+            let key = n as keyof typeof endpoint;
+            if(typeof property == typeof endpoint[key]){
+                endpoint[key]?.push(property);
+            }
+        })
+        return endpoint;
+    }*/
+}
